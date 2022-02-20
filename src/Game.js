3bc4d8a55add5cf73cd63b8464dbf09b855298c3
18616781863 <98248920+18616781863@users.noreply.github.com>
2022-02-20 13:22:26 +0800

    Add files via upload

---
+++ src/Game.js
@@ -0,0 +1,165 @@
+import TileMap from "./TileMap.js";
+
+const canvas = document.getElementById("gameCanvas");
+
+canvas.width = 500;
+canvas.height = 500;
+const tileCountX = 5;
+const tileSizeX = canvas.width / tileCountX - 10;
+const gapX = (canvas.width - tileSizeX * tileCountX) / (tileCountX + 1);
+const tileCountY = 5;
+const tileSizeY = canvas.height / tileCountY - 10;
+const gapY = (canvas.height - tileSizeY * tileCountY) / (tileCountY + 1);
+const renderSpeed = 10;
+
+const tileMap = new TileMap(tileSizeX, gapX, tileSizeY, gapY);
+tileMap.init();
+
+let gameWin = false;
+let gameOver = false;
+const gameWinSound = new Audio("/sounds/gameWin.wav");
+const gameOverSound = new Audio("/sounds/gameOver.wav");
+
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const startButton = document.querySelector(".startButton");
+const reloadButton = document.querySelector(".reloadButton");
+startButton.addEventListener("click", startGame);
+reloadButton.addEventListener("click", reload);
+
+document.getElementById("timerLabel").innerHTML =
+  "倒计时 -" + Array(1).fill("\xa0").join("");
+
+let timerActive = false;
+let timeArr = [];
+let min = 5;
+let sec = 0;
+if (min < 10) min = "0" + min;
+if (sec < 10) sec = "0" + sec;
+document.getElementById("timer").innerHTML = min + ":" + sec;
+
+function startGame() {
+  startButton.removeEventListener("click", startGame);
+  canvas.addEventListener("click", getPos);
+  timerActive = true;
+  startTimer();
+}
+
+gameLoop();
+
+function gameLoop() {
+  if (gameOver || gameWin) {
+    drawGameEnd();
+    return;
+  }
+  tileMap.draw(ctx);
+  checkGameWin();
+  checkGameOver();
+
+  setTimeout(gameLoop, 1000 / renderSpeed);
+}
+
+function checkGameOver() {
+  if (!gameOver) {
+    gameOver = isGameOver();
+    if (gameOver) {
+      gameOverSound.play();
+    }
+  }
+  return gameOver;
+}
+
+function checkGameWin() {
+  if (!gameWin) {
+    gameWin = isGameWin();
+    if (gameWin) {
+      gameWinSound.play();
+    }
+  }
+  return gameWin;
+}
+
+function isGameOver() {
+  if (min == 0 && sec == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isGameWin() {
+  if (tileMap.map.flat().toString() == tileMap.mapWin.flat().toString()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function drawGameEnd() {
+  let text = "";
+  if (gameWin) {
+    // text = "  You Win!";
+    document.getElementById("result").innerHTML =
+      Array(10).fill("\xa0").join("") +
+      Array(3).fill("😎").join("") +
+      "成功" +
+      Array(3).fill("😎").join("");
+    // Array(5).fill("\xa0").join("") + "成功" + Array(5).fill("\xa0").join("");
+  } else if (gameOver) {
+    // text = "Game Over";
+    document.getElementById("result").innerHTML =
+      Array(10).fill("\xa0").join("") +
+      Array(3).fill("😒").join("") +
+      "失败" +
+      Array(3).fill("😒").join("");
+    // Array(5).fill("\xa0").join("") + "失败" + Array(5).fill("\xa0").join("");
+  }
+
+  // ctx.font = "50px Verdana";
+  // const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+  // gradient.addColorStop("0", "magenta");
+  // gradient.addColorStop("0.5", "blue");
+  // gradient.addColorStop("1.0", "red");
+  // ctx.fillStyle = "white";
+  // ctx.fillText(text, canvas.width / 6.5, canvas.height / 1.9);
+}
+
+function startTimer() {
+  if (timerActive === true) {
+    timeArr = document.getElementById("timer").innerHTML.split(":");
+    min = timeArr[0];
+    sec = timeArr[1];
+
+    if (sec == 0) {
+      min--;
+      if (min < 10) {
+        min = "0" + min;
+      }
+      sec = "59";
+    } else {
+      sec--;
+      if (sec < 10) {
+        sec = "0" + sec;
+      }
+    }
+
+    document.getElementById("timer").innerHTML = min + ":" + sec;
+    if ((min == 0 && sec == 0) || gameWin) return;
+    setTimeout(startTimer, 1000);
+  }
+}
+
+function reload() {
+  reload = location.reload();
+}
+
+function getPos(e) {
+  let column = Math.floor(e.layerX / (tileSizeX + gapX));
+  if (column >= tileCountX) column = tileCountX - 1;
+  let row = Math.floor(e.layerY / (tileSizeY + gapY));
+  if (row >= tileCountY) row = tileCountY - 1;
+  // console.log(e.layerX, e.layerY, column, row);
+  tileMap.change(row, column);
+}
---
+++ src/TileMap.js
@@ -0,0 +1,414 @@
+export default class TileMap {
+  constructor(tileSizeX, gapX, tileSizeY, gapY) {
+    this.tileSizeX = tileSizeX;
+    this.gapX = gapX;
+    this.tileSizeY = tileSizeY;
+    this.gapY = gapY;
+    this.column0 = this.map.length - 1;
+    this.row0 = this.map[0].length - 1;
+    this.initStep = 100;
+  }
+
+  changeSound = new Audio("/sounds/change.mp3");
+
+  // 5 X 5 map
+  map = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 0],
+  ];
+
+  mapWin = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 0],
+  ];
+
+  draw(ctx) {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        switch (this.map[row][column]) {
+          case 0:
+            ctx.fillStyle = "black";
+            break;
+          default:
+            ctx.fillStyle = "grey";
+        }
+        ctx.fillRect(
+          column * (this.tileSizeX + this.gapX) + this.gapX,
+          row * (this.tileSizeY + this.gapY) + this.gapY,
+          this.tileSizeX,
+          this.tileSizeY
+        );
+        ctx.font = "30px Verdana";
+        ctx.fillStyle = "white";
+        if (this.map[row][column] >= 10) {
+          ctx.fillText(
+            this.map[row][column],
+            column * (this.tileSizeX + this.gapX) + this.tileSizeX / 2 - 10,
+            row * (this.tileSizeY + this.gapY) + this.tileSizeY / 1.5
+          );
+        } else if (this.map[row][column] > 0) {
+          ctx.fillText(
+            this.map[row][column],
+            column * (this.tileSizeX + this.gapX) + this.tileSizeX / 2,
+            row * (this.tileSizeY + this.gapY) + this.tileSizeY / 1.5
+          );
+        }
+      }
+    }
+  }
+
+  change(row, column) {
+    let columnMax = this.map[row].length - 1;
+    let rowMax = this.map.length - 1;
+    let a = this.map[row][column];
+
+    switch (true) {
+      // row == rowMax
+      case row == rowMax && column == columnMax:
+        if (this.map[row - 1][column] == 0) {
+          this.map[row - 1][column] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row][column - 1] == 0) {
+          this.map[row][column - 1] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        }
+        break;
+      case row == rowMax && column > 0 && column < columnMax:
+        if (this.map[row - 1][column] == 0) {
+          this.map[row - 1][column] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row][column + 1] == 0) {
+          this.map[row][column + 1] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row][column - 1] == 0) {
+          this.map[row][column - 1] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        }
+        break;
+      case row == rowMax && column == 0:
+        if (this.map[row - 1][column] == 0) {
+          this.map[row - 1][column] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row][column + 1] == 0) {
+          this.map[row][column + 1] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        }
+        break;
+      // row == 0
+      case row == 0 && column == columnMax:
+        if (this.map[row + 1][column] == 0) {
+          this.map[row + 1][column] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row][column - 1] == 0) {
+          this.map[row][column - 1] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        }
+        break;
+      case row == 0 && column > 0 && column < columnMax:
+        if (this.map[row + 1][column] == 0) {
+          this.map[row + 1][column] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row][column + 1] == 0) {
+          this.map[row][column + 1] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row][column - 1] == 0) {
+          this.map[row][column - 1] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        }
+        break;
+      case row == 0 && column == 0:
+        if (this.map[row + 1][column] == 0) {
+          this.map[row + 1][column] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row][column + 1] == 0) {
+          this.map[row][column + 1] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        }
+        break;
+      // row > 0 && row < rowMax
+      case row > 0 && row < rowMax && column == columnMax:
+        if (this.map[row + 1][column] == 0) {
+          this.map[row + 1][column] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row - 1][column] == 0) {
+          this.map[row - 1][column] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row][column - 1] == 0) {
+          this.map[row][column - 1] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        }
+        break;
+      case row > 0 && row < rowMax && column > 0 && column < columnMax:
+        if (this.map[row + 1][column] == 0) {
+          this.map[row + 1][column] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row - 1][column] == 0) {
+          this.map[row - 1][column] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row][column + 1] == 0) {
+          this.map[row][column + 1] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row][column - 1] == 0) {
+          this.map[row][column - 1] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        }
+        break;
+      case row > 0 && row < rowMax && column == 0:
+        if (this.map[row + 1][column] == 0) {
+          this.map[row + 1][column] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        }
+        if (this.map[row - 1][column] == 0) {
+          this.map[row - 1][column] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        } else if (this.map[row][column + 1] == 0) {
+          this.map[row][column + 1] = a;
+          this.map[row][column] = 0;
+          this.changeSound.play();
+        }
+        break;
+    }
+  }
+
+  init() {
+    for (let i = 0; i < this.initStep; i++) {
+      this.#move(this.row0, this.column0);
+      // console.log(this.row0, this.column0);
+    }
+  }
+
+  #move(row, column) {
+    let columnMax = this.map[0].length - 1;
+    let rowMax = this.map.length - 1;
+    let i = 0;
+    switch (true) {
+      // middle
+      case row > 0 && row < rowMax && column > 0 && column < columnMax:
+        i = Math.floor(Math.random() * 4 + 1);
+        // 1 up
+        // 2 down
+        // 3 left
+        // 4 right
+        switch (i) {
+          case 1:
+            this.map[row][column] = this.map[row - 1][column];
+            this.map[row - 1][column] = 0;
+            this.row0--;
+            break;
+          case 2:
+            this.map[row][column] = this.map[row + 1][column];
+            this.map[row + 1][column] = 0;
+            this.row0++;
+            break;
+          case 3:
+            this.map[row][column] = this.map[row][column - 1];
+            this.map[row][column - 1] = 0;
+            this.column0--;
+            break;
+          case 4:
+            this.map[row][column] = this.map[row][column + 1];
+            this.map[row][column + 1] = 0;
+            this.column0++;
+            break;
+        }
+        break;
+      // right bottom
+      case row == rowMax && column == columnMax:
+        i = Math.floor(Math.random() * 2 + 1);
+        // 1 up
+        // 2 left
+        switch (i) {
+          case 1:
+            this.map[row][column] = this.map[row - 1][column];
+            this.map[row - 1][column] = 0;
+            this.row0--;
+            break;
+          case 2:
+            this.map[row][column] = this.map[row][column - 1];
+            this.map[row][column - 1] = 0;
+            this.column0--;
+            break;
+        }
+        break;
+      // left bottom
+      case row == rowMax && column == 0:
+        i = Math.floor(Math.random() * 2 + 1);
+        // 1 up
+        // 2 right
+        switch (i) {
+          case 1:
+            this.map[row][column] = this.map[row - 1][column];
+            this.map[row - 1][column] = 0;
+            this.row0--;
+            break;
+          case 2:
+            this.map[row][column] = this.map[row][column + 1];
+            this.map[row][column + 1] = 0;
+            this.column0++;
+            break;
+        }
+        break;
+      // right top
+      case row == 0 && column == columnMax:
+        i = Math.floor(Math.random() * 2 + 1);
+        // 1 down
+        // 2 left
+        switch (i) {
+          case 1:
+            this.map[row][column] = this.map[row + 1][column];
+            this.map[row + 1][column] = 0;
+            this.row0++;
+            break;
+          case 2:
+            this.map[row][column] = this.map[row][column - 1];
+            this.map[row][column - 1] = 0;
+            this.column0--;
+            break;
+        }
+        break;
+      // left top
+      case row == 0 && column == 0:
+        i = Math.floor(Math.random() * 2 + 1);
+        // 1 down
+        // 2 right
+        switch (i) {
+          case 1:
+            this.map[row][column] = this.map[row + 1][column];
+            this.map[row + 1][column] = 0;
+            this.row0++;
+            break;
+          case 2:
+            this.map[row][column] = this.map[row][column + 1];
+            this.map[row][column + 1] = 0;
+            this.column0++;
+            break;
+        }
+        break;
+      // middle bottom
+      case row == rowMax && column > 0 && column < columnMax:
+        i = Math.floor(Math.random() * 3 + 1);
+        // 1 up
+        // 2 left
+        // 3 right
+        switch (i) {
+          case 1:
+            this.map[row][column] = this.map[row - 1][column];
+            this.map[row - 1][column] = 0;
+            this.row0--;
+            break;
+          case 2:
+            this.map[row][column] = this.map[row][column - 1];
+            this.map[row][column - 1] = 0;
+            this.column0--;
+            break;
+          case 3:
+            this.map[row][column] = this.map[row][column + 1];
+            this.map[row][column + 1] = 0;
+            this.column0++;
+            break;
+        }
+        break;
+      // middle right
+      case row > 0 && row < rowMax && column == columnMax:
+        i = Math.floor(Math.random() * 3 + 1);
+        // 1 up
+        // 2 down
+        // 3 left
+        switch (i) {
+          case 1:
+            this.map[row][column] = this.map[row - 1][column];
+            this.map[row - 1][column] = 0;
+            this.row0--;
+            break;
+          case 2:
+            this.map[row][column] = this.map[row + 1][column];
+            this.map[row + 1][column] = 0;
+            this.row0++;
+            break;
+          case 3:
+            this.map[row][column] = this.map[row][column - 1];
+            this.map[row][column - 1] = 0;
+            this.column0--;
+            break;
+        }
+        break;
+      // middle top
+      case row == 0 && column > 0 && column < columnMax:
+        i = Math.floor(Math.random() * 3 + 1);
+        // 1 down
+        // 2 left
+        // 3 right
+        switch (i) {
+          case 1:
+            this.map[row][column] = this.map[row + 1][column];
+            this.map[row + 1][column] = 0;
+            this.row0++;
+            break;
+          case 2:
+            this.map[row][column] = this.map[row][column - 1];
+            this.map[row][column - 1] = 0;
+            this.column0--;
+            break;
+          case 3:
+            this.map[row][column] = this.map[row][column + 1];
+            this.map[row][column + 1] = 0;
+            this.column0++;
+            break;
+        }
+        break;
+      // middle left
+      case row > 0 && row < rowMax && column == 0:
+        i = Math.floor(Math.random() * 3 + 1);
+        // 1 up
+        // 2 down
+        // 3 right
+        switch (i) {
+          case 1:
+            this.map[row][column] = this.map[row - 1][column];
+            this.map[row - 1][column] = 0;
+            this.row0--;
+            break;
+          case 2:
+            this.map[row][column] = this.map[row + 1][column];
+            this.map[row + 1][column] = 0;
+            this.row0++;
+            break;
+          case 3:
+            this.map[row][column] = this.map[row][column + 1];
+            this.map[row][column + 1] = 0;
+            this.column0++;
+            break;
+        }
+        break;
+    }
+  }
+}
